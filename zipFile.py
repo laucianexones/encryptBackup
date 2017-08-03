aa3118d7cfc6c344c7704da3edb9bf12a3d60702
laucianexones <keremcumhur@gmail.com>
2017-08-03 14:33:22 +0200

    Update zipFile.py
    dosyaları relpath a dikkat edecek şekilde, subfolder ağacına dikkat edecek şekilde sıkıştırıyorum.

--- zipFile.py
+++ zipFile.py
@@ -1,12 +1,24 @@
 import os
 import zipfile
 
-path =  r'C:\Users\kc\Desktop\_tmp\rootFolder'
+
+path = r'C:\Users\kcumhurx\Desktop\na training'
+target_file = '%s.zip' % path
+
+zipObject = zipfile.ZipFile(target_file,'w',zipfile.ZIP_DEFLATED)
 
 def zipdir(path):
-  for root, dirs, files in os.walk(path):
-    print dirs
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            fullpath_filename = os.path.join(root, file)
+            #print fullpath_filename
+            abs_filename = fullpath_filename.replace(path, '')
+            #print abs_filename
+                       
+            zipObject.write(fullpath_filename, abs_filename)
 
-if __name__ == '__main__':
-  #zipf = zipfile.Zipfile('test.zip','w',zipfile.ZIP_DEFLATED)
-  zipdir(path)
+    zipObject.close()
+    print "%s created" % target_file
+    
+
+zipdir(path)
