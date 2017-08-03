5890c14f5bef3a2c879b2d6570aaa1d83d24b3a7
laucianexones <keremcumhur@gmail.com>
2017-08-03 16:12:50 +0200

    Update zipFile.py
    help to split files

--- zipFile.py
+++ zipFile.py
@@ -22,3 +22,34 @@ def zipdir(path):
     
 
 zipdir(path)
+
+"""
+import os
+import stat
+import zipfile
+
+archive_num = 1
+outfile = zipfile.ZipFile('/zips/archive%s.zip' % archive_num, "w")
+zsize = 0
+
+for full_name in filelist:
+    full_name_path = os.path.join(full_name, full_path)
+    if outfile.fp.tell() > 15728640: # 15mb
+        outfile.close()
+        archive_num += 1
+        outfile = zipfile.ZipFile('/zips/archive%s.zip' % archive_num, "w")
+
+    outfile.write(full_name_path,full_name_path,zipfile.ZIP_DEFLATED)
+
+
+There is still a couple of "issues":
+
+1) Files larger than 15Mb may not be able to be compressed to fall below the limit.
+
+2) If you are near the 15Mb limit and the next file is very large you have the
+same problem.
+
+
+-Larry
+
+"""
