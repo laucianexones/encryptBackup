2e2ffa13cb55d50d4cf5070bccdc0a6baf6576fe
Kerem <keremcumhur@gmail.com>
2017-08-05 01:40:11 +0200

    Revert "yallah"
    This reverts commit 20858eedf82dba8b5efc28073316048fa2f40d70.

--- zipFile.py
+++ zipFile.py
@@ -1,14 +1,60 @@
 import os
 import zipfile
 
-path = r'C:\Users\kc\Desktop\_tmp\Saved Pictures'
 
-def zipdir(path, ziph):
-  for root, dirs, files in os.walk(path):
-    for file in files:
-      ziph.write(os.path.join(root,file))
+path = r'C:\Users\kcumhurx\Desktop\na training'
+target_file = '%s.zip' % path
+pwd = b'kerem'
+print type(pwd)
+LIMIT_SIZE = 10485760 # 10MB
 
-if __name__ == '__main__':
-  zipf = zipfile.ZipFile('test.zip', 'w', zipfile.ZIP_DEFLATED)
-  zipdir(path, zipf)
-  zipf.close()
+zipObject = zipfile.ZipFile(target_file,'w',zipfile.ZIP_DEFLATED)
+zipObject.setpassword(pwd)
+
+def convert_bytes(num):
+    """
+    this function will convert bytes to MB.... GB... etc
+    """
+    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0
+
+num = 0
+def zipdir(path, zipObject, num):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            fullpath_filename = os.path.join(root, file)
+            #print fullpath_filename
+            abs_filename = fullpath_filename.replace(path, '')
+            #print abs_filename
+
+            print type(zipObject)            
+            file_size = zipObject.fp.tell()
+            if not file_size >= LIMIT_SIZE:
+                zipObject.write(fullpath_filename, abs_filename)
+                print '%s compressed' % abs_filename
+                print zipObject.fp.tell()
+                print '\n'
+            else:
+                print 'new archieve file'
+                zipObject.close()
+                num += 1
+                zipObject = zipfile.ZipFile('%s_%s.zip' % (path, num), 'w', zipfile.ZIP_DEFLATED)
+                zipObject.write(fullpath_filename, abs_filename)
+                print '%s compressed' % abs_filename
+                print zipObject.fp.tell()
+                print '\n'
+
+    
+    file_size = zipObject.fp.tell()
+    file_size_human_readable = convert_bytes(file_size)
+    
+    print 'size is: ', file_size_human_readable
+    zipObject.close()
+    print "%s created" % target_file
+    
+
+
+zipdir(path, zipObject, num)
+
