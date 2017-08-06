571729b16fb53d1ca35ec45789a6200861dc5a86
Kerem <keremcumhur@gmail.com>
2017-08-06 23:30:32 +0200

    encrypt & decrypt - reader.py
    reader.py dosyası içinde AES encrypt - decrypt

--- reader.py
+++ reader.py
@@ -1,10 +1,37 @@
 # -*- encoding: utf-8 -*-
 import os
+# pip install pycryptodomex
+from Cryptodome.Cipher import AES
+from Cryptodome.Protocol.KDF import scrypt
+#from Crypto.Random import get_random_bytes
 
-s = "ğüıç"
-print type(s)
-print s
+AESKEYLEN = 16
+SALTLEN = 16
+NUMBEROFKEYS = 1
+SCRYPT_N, SCRYPT_r, SCRYPT_p = 1048576, 8, 1
 
-us = s.decode('utf-8')
-print type(us)
-print us
+def transportsafe(mystring):
+    return mystring.encode('utf-8')
+
+password = transportsafe("Aglama Dalai Lama")
+salt = os.urandom(SALTLEN)
+
+key = scrypt(password, salt, AESKEYLEN, SCRYPT_N, SCRYPT_r, SCRYPT_p, NUMBEROFKEYS)
+cipher = AES.new(key, AES.MODE_EAX)
+
+data = transportsafe("this is a super secret text")
+ciphertext, tag = cipher.encrypt_and_digest(data)
+file_out = open("encrypted.bin", "wb")
+[file_out.write(x) for x in cipher.nonce, tag, ciphertext]
+file_out.close()
+
+print 'encrypted text : %s' % ciphertext
+
+
+file_in = open("encrypted.bin", "rb")
+nonce, tag, ciphertext = [ file_in.read(x) for x in (16, 16, -1) ]
+
+cipher = AES.new(key, AES.MODE_EAX, nonce)
+decrypted_data = cipher.decrypt_and_verify(ciphertext, tag)
+
+print 'decrypted text : %s' % decrypted_data
