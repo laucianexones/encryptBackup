04037ebd497b8b3ad8c47d449e745d3c50702087
Kerem <keremcumhur@gmail.com>
2019-06-02 16:19:32 +0200

    added features

--- calculate_weight.py
+++ calculate_weight.py
@@ -2,7 +2,10 @@ import os,sys
 import pprint
 import operator
 overall_dict = {}
+#TODO enable parameter for pretty print option
+pretty_print = False
 
+#TODO write a proper usage text
 def usage():
     print 'run the script with a valid foldername'
 
@@ -14,51 +17,83 @@ def check_startpath(s_path):
 
 def get_filesize(filename):
     size = os.stat(filename).st_size
-    return size
+    return size 
 
-#TODO
-def accumulate_parent_weight(current_dir,parent,folder_size_sum):
-    # write the calculated size of the current_dir to the local dict
-    # if the folder has a parent, add the folder size to the parent as well
-    if not overall_dict.has_key(current_dir):
-        overall_dict[current_dir] = folder_size_sum
-    else:
-        # this else clause should not happen
-        print '#DEBUG ELSE'
-        overall_dict[current_dir] = overall_dict[current_dir] + folder_size_sum
+def create_database(folder_path,num_of_files,num_of_subfolders,folder_size_sum,level,parent,depth):
+    if not overall_dict.has_key(folder_path):
+        overall_dict[folder_path] = [num_of_files,folder_size_sum,level,parent,num_of_subfolders]
+        if level > depth:
+            depth = level
+    return depth
 
-    if not overall_dict.has_key(parent):
-        overall_dict[parent] = folder_size_sum
-    else:
-        overall_dict[parent] = overall_dict[parent] + folder_size_sum
-    pprint.pprint(overall_dict)
-    
+
+def recreate_database(depth):
+    # take as argument the depth of the tree
+    # make a bottom-up iteration over the tree represented in the overall_dict variable
+    # use range(depth,-1,-1) to start from the last level # range(5,-1,-1) results to [5, 4, 3, 2, 1, 0]
+    # for item in overall_dict:
+    #     for i in range(depth,-1,-1):
+    for i in range(depth,-1,-1):
+        for item in overall_dict:
+            # if the item of the dictionary has the depth-level we are looking for
+            # get the parent item of the current folder
+            # add current folders size to the parent
+            if overall_dict[item][2] == i:
+                parent = overall_dict[item][3]
+                if parent == None:
+                    continue
+                folder_size = overall_dict[item][1]
+                overall_dict[parent][1] += folder_size
+
 
 def print_dict_sorted(dict):
-    sorted_dict = sorted(dict.items(),key=operator.itemgetter(1))
-    print 'sorting...'
-    pprint.pprint(sorted_dict)
+    sorted_dict_as_list = sorted(dict.items(),key=lambda x:x[1][2])
+    l_level = 0
+    for i in sorted_dict_as_list:
+        level = i[1][2]
+        sizeGB = i[1][1] / (1024*1024*1024)
+        num_of_files = i[1][0]
+        num_of_subfolders = i[1][4]
+        folder_path = i[0]
+
+        #TODO remove parent information & calculate weight
+        #subfolder number relevant for weight ??
+
+        if level > l_level:
+            print '--'
+            l_level = level
+        print 'sizeGB:%s\t#_files:%s\t#_subfolders:%s\tlevel:%s\t\tpath:%s' % (sizeGB,num_of_files,num_of_subfolders,level,folder_path)
 
 
-#TODO rename the function - confusing name
-def list_files(startpath):
-    print 'W:Weight S:Size in KB N:Num of files SF: Num of Subfolders'
-    print startpath
-    print '##'*20
-    for root, dirs, files in os.walk(startpath):
-        level = root.replace(startpath, '').count(os.sep)
-        current_dir = root.replace(startpath,'').split(os.sep)[-1]
-        parent = root.replace(current_dir,'')
+
+def scan_filesystem(startpath,tree_depth):
+    '''
+    scans the given file system and creates a database with number of files, folder size, level and parents
+    '''
+    if pretty_print:
+        print 'W:Weight S:Size in KB N:Num of files SF: Num of Subfolders'
+        print startpath
+        print '##'*20
+    
+    for folder_path, dirs, files in os.walk(startpath):
+        level = folder_path.replace(startpath, '').count(os.sep) +1
+        current_dir = folder_path.replace(startpath,'').split(os.sep)[-1]
+        #parent = folder_path.replace(current_dir,'').rstrip('/')
+        parent = os.sep.join(folder_path.split(os.sep)[:-1])
         if current_dir == '':
             current_dir = '/'
             parent = None
-        print '#parent : ', parent
+            level = 0
+        if folder_path == startpath:
+            folder_path = folder_path.rstrip('/')
         
-        # DEBUG Commands : use them to understand the loop step
+        #DEBUG Commands : use them to understand the loop step
+        # print 'folder_path :', folder_path
         # print 'current_dir is :', current_dir
         # print 'level is : ', level
         # print 'subdirs are :', dirs
         # print 'files are :', files
+        # print 'parent :' , parent
 
         # this for-loop iterates over the current directory
         # checks the size of each file #TODO check for other filetypes like links,hardlinks etc..
@@ -67,35 +102,28 @@ def list_files(startpath):
         # using these 2 variables a weight is created
         folder_size_sum = 0        
         for f in files:
-            f_fullpath = os.path.join(root,f)
+            f_fullpath = os.path.join(folder_path,f)
             # skip the file if it is a symlink
             if os.path.islink(f_fullpath):
                 continue
             f_size = get_filesize(f_fullpath)
             folder_size_sum += f_size
-        # calculate the weight of the folder
+        # get the number of files in the current folder
         num_of_files = len(files)
-        weight = float((num_of_files * folder_size_sum) / 1024)
-
         # get the number of subfolders in the current folders
         num_of_subfolders = len(dirs)
 
         # print the current dir and related information 
         # use indent var. in the formatting to show the depth of the folder
-        indent = ' ' * 4 * (level)
-        print('{}{} - W:{} S:{} N:{} SF:{}'.format(indent, current_dir, weight, folder_size_sum, num_of_files,num_of_subfolders))
+        if pretty_print:
+            indent = '-' * 4 * (level)
+            print('{}{} - W:{} S:{} N:{} SF:{}'.format(indent, current_dir, weight, folder_size_sum, num_of_files,num_of_subfolders))
         
-        #TODO fix the parent & current folder names
-        #parent :  /Users/kerem/Desktop/testFolder2/level1-F3/
-        # L2-F3 - W:174.0 S:22308 N:8 SF:1
-        # {None: 0,
-        # '/Users/kerem/Desktop/testFolder2/': 22392,
-        # '/Users/kerem/Desktop/testFolder2/level1-F3': 22392,
-        # '/Users/kerem/Desktop/testFolder2/level1-F3/': 22308,
-        # '/Users/kerem/Desktop/testFolder2/level1-F3/L2-F3': 22308}
+        tree_depth = create_database(folder_path,num_of_files,num_of_subfolders,folder_size_sum,level,parent,tree_depth)
+        #print '-'*20
 
-        accumulate_parent_weight(root,parent,folder_size_sum)
-        print '-'*20
+    return tree_depth
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -103,11 +131,10 @@ if __name__ == '__main__':
         exit(1)
     startpath = sys.argv[1]
     if check_startpath(startpath):
-       list_files(startpath)
+        tree_depth = 0
+        tree_depth = scan_filesystem(startpath,tree_depth)
     else:
         print '%s is not a valid directory path' % startpath
 
+    recreate_database(tree_depth)
     print_dict_sorted(overall_dict)
-
-#startpath = r'/Users/kerem/Desktop/testFolder2/'
-#list_files(startpath)
