c418d65e83119bb8e498272985e10ef07666ea0b
Kerem <keremcumhur@gmail.com>
2019-06-02 21:17:37 +0200

    local variables removed

--- calculate_weight.py
+++ calculate_weight.py
@@ -1,8 +1,7 @@
 import os,sys
 import pprint
-import operator
-overall_dict = {}
-top_folders = []
+#import operator
+
 #TODO enable parameter for pretty print option
 pretty_print = False
 
@@ -20,34 +19,35 @@ def get_filesize(filename):
     size = os.stat(filename).st_size
     return size 
 
-def create_database(folder_path,num_of_files,num_of_subfolders,folder_size_sum,level,parent,depth):
-    if not overall_dict.has_key(folder_path):
-        overall_dict[folder_path] = [num_of_files,folder_size_sum,level,parent,num_of_subfolders]
+def create_database(folder_path,num_of_files,num_of_subfolders,folder_size_sum,level,parent,depth,fs_tree):
+
+    if not fs_tree.has_key(folder_path):
+        fs_tree[folder_path] = [num_of_files,folder_size_sum,level,parent,num_of_subfolders]
         if level > depth:
             depth = level
-    return depth
+    return depth,fs_tree
 
 
-def recreate_database(depth):
+def recreate_database(depth,fs_tree):
     # take as argument the depth of the tree
-    # make a bottom-up iteration over the tree represented in the overall_dict variable
+    # make a bottom-up iteration over the tree represented in the fs_tree variable
     # use range(depth,-1,-1) to start from the last level # range(5,-1,-1) results to [5, 4, 3, 2, 1, 0]
     # for item in overall_dict:
     #     for i in range(depth,-1,-1):
     for i in range(depth,-1,-1):
-        for item in overall_dict:
+        for item in fs_tree:
             # if the item of the dictionary has the depth-level we are looking for
             # get the parent item of the current folder
             # add current folders size to the parent
-            if overall_dict[item][2] == i:
-                parent = overall_dict[item][3]
+            if fs_tree[item][2] == i:
+                parent = fs_tree[item][3]
                 if parent == None:
                     continue
-                folder_size = overall_dict[item][1]
-                overall_dict[parent][1] += folder_size
+                folder_size = fs_tree[item][1]
+                fs_tree[parent][1] += folder_size
+    return fs_tree
 
-
-def print_dict_sorted(dict):
+def print_dict_sorted(dict,top_folders):
     sorted_dict_as_list = sorted(dict.items(),key=lambda x:x[1][2])
     l_level = 0
     for i in sorted_dict_as_list:
@@ -60,24 +60,33 @@ def print_dict_sorted(dict):
         #TODO remove parent information & calculate weight
         #subfolder number relevant for weight ??
 
+        # normalize the values to use in the weight calculation formula
         values_for_weight = [sizeGB,num_of_files,num_of_subfolders]
         for i in values_for_weight:
             if i == 0:
                 ind = values_for_weight.index(i)
                 values_for_weight[ind] = 1
-
-        weight = values_for_weight[0] * values_for_weight[1] * values_for_weight[2]
+        f_folder_size, f_num_of_files, f_num_subfolders = values_for_weight 
+        # normalize the number of files by dividing to 100
+        weight = f_folder_size * (f_num_of_files/100) * f_num_subfolders
 
 
         top_folders.append([weight,sizeGB,num_of_files,num_of_subfolders,folder_path])
         if level > l_level:
             print '--'
             l_level = level
-        print 'WeightScore:%s\t\tsizeGB:%s\t#_files:%s\t#_subfolders:%s\tlevel:%s\t\tpath:%s' % (weight,sizeGB,num_of_files,num_of_subfolders,level,folder_path)
+        print 'WeightScore:%s\t\t\tsizeGB:%s\t#_files:%s\t#_subfolders:%s\tlevel:%s\t\tpath:%s' % (weight,sizeGB,num_of_files,num_of_subfolders,\
+            level,folder_path)
+    
+    print '--'*20
+    print '--'*20
+    print 'printing the top-10 weighted folders..'
+    print 'weight,sizeGB,#_files,#_subfolders'
+    for i in sorted(top_folders)[::-1][:10]:
+        print i
 
 
-
-def scan_filesystem(startpath,tree_depth):
+def scan_filesystem(startpath,tree_depth,fs_tree):
     '''
     scans the given file system and creates a database with number of files, folder size, level and parents
     '''
@@ -97,14 +106,6 @@ def scan_filesystem(startpath,tree_depth):
             level = 0
         if folder_path == startpath:
             folder_path = folder_path.rstrip('/')
-        
-        #DEBUG Commands : use them to understand the loop step
-        # print 'folder_path :', folder_path
-        # print 'current_dir is :', current_dir
-        # print 'level is : ', level
-        # print 'subdirs are :', dirs
-        # print 'files are :', files
-        # print 'parent :' , parent
 
         # this for-loop iterates over the current directory
         # saves the total size of the folder to folder_size_sum
@@ -130,10 +131,10 @@ def scan_filesystem(startpath,tree_depth):
             indent = '-' * 4 * (level)
             print('{}{} - W:{} S:{} N:{} SF:{}'.format(indent, current_dir, weight, folder_size_sum, num_of_files,num_of_subfolders))
         
-        tree_depth = create_database(folder_path,num_of_files,num_of_subfolders,folder_size_sum,level,parent,tree_depth)
+        tree_depth,fs_tree = create_database(folder_path,num_of_files,num_of_subfolders,folder_size_sum,level,parent,tree_depth,fs_tree)
         #print '-'*20
 
-    return tree_depth
+    return tree_depth,fs_tree
 
 
 if __name__ == '__main__':
@@ -141,18 +142,13 @@ if __name__ == '__main__':
         usage()
         exit(1)
     startpath = sys.argv[1]
+    #do the scan of the filesystem
     if check_startpath(startpath):
-        tree_depth = scan_filesystem(startpath,tree_depth=0)
+        tree_depth,fs_tree = scan_filesystem(startpath,tree_depth=0,fs_tree={})
     else:
         print '%s is not a valid directory path' % startpath
-
-    recreate_database(tree_depth)
-    print_dict_sorted(overall_dict)
-
-    print '--'*20
-    print '--'*20
-    print 'printing the top-10 weighted folders..'
-    print 'weight,sizeGB,#_files,#_subfolders'
-    for i in sorted(top_folders)[::-1][:10]:
-        print i
+    #do the bottom-up sorting & size calculation of the filesystem tree
+    fs_tree = recreate_database(tree_depth,fs_tree)
     
+    #print the tree by levels & top-10 weighted directories
+    print_dict_sorted(fs_tree,top_folders=[])
